97e2bac4a0ee4af21c8fd7e6f8a7f6ed8c4a0cb0
Shailendra Pathak <153452113+PathakShailendra@users.noreply.github.com>
2024-11-10 17:42:35 +0530

    Add files via upload
    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function navAnimation(){
+    document.querySelector('body').addEventListener('wheel' , function(dets){
+        if(dets.deltaY > 0){
+            gsap.to('nav',   {
+                duration: 0.25,
+                transform: `translateX(-50%) translateY(-150%)`
+            })
+        }
+        else {
+            gsap.to('nav',   {
+                duration: 0.25,
+                transform: `translateX(-50%) translateY(0%)`
+            })
+        }
+    })
+}
